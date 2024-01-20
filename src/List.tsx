9c0eca59580f4fef2361f61b87b05fa75e794e42
Daritork <87564532+Daritork@users.noreply.github.com>
2024-01-20 22:15:52 +0100

    tsx update

--- src/List.tsx
+++ src/List.tsx
@@ -1,4 +1,13 @@
 import { useState } from "react";
+import Sopranino from "./images/saxTypes/Sopranino.png";
+import Studio_Soprano from "./images/saxTypes/Sopranino.png";
+import Curved_Soprano from "./images/saxTypes/Sopranino.png";
+import Alto from "./images/saxTypes/Sopranino.png";
+import Tenor from "./images/saxTypes/Sopranino.png";
+import Baritone from "./images/saxTypes/Sopranino.png";
+import Bass from "./images/saxTypes/Sopranino.png";
+import Contrabass from "./images/saxTypes/Sopranino.png";
+import C from "./images/saxTypes/Sopranino.png";
 
 function List() {
   const [saxTypes] = useState<
@@ -8,17 +17,67 @@ function List() {
       heightCm?: number;
       heightIn?: number;
       pitch: string;
+      imgPath: any;
     }[]
   >([
-    { id: 1, name: "Sopranino", pitch: "E♭", heightCm: 33, heightIn: 13 },
-    { id: 2, name: "Studio Soprano", pitch: "B♭", heightCm: 71, heightIn: 28 },
-    { id: 3, name: "Curved Soprano", pitch: "B♭", heightCm: 46, heightIn: 18 },
-    { id: 4, name: "Alto", pitch: "E♭", heightCm: 74, heightIn: 29 },
-    { id: 5, name: "Tenor", pitch: "B♭", heightCm: 94, heightIn: 37 },
-    { id: 6, name: "Baritone", pitch: "E♭", heightCm: 132, heightIn: 52 },
-    { id: 7, name: "Bass", pitch: "B♭" },
-    { id: 8, name: "Contrabass", pitch: "E♭", heightCm: 200, heightIn: 79 },
-    { id: 9, name: "C -", pitch: "C" },
+    {
+      id: 1,
+      name: "Sopranino",
+      pitch: "E♭",
+      heightCm: 33,
+      heightIn: 13,
+      imgPath: Sopranino,
+    },
+    {
+      id: 2,
+      name: "Studio Soprano",
+      pitch: "B♭",
+      heightCm: 71,
+      heightIn: 28,
+      imgPath: Studio_Soprano,
+    },
+    {
+      id: 3,
+      name: "Curved Soprano",
+      pitch: "B♭",
+      heightCm: 46,
+      heightIn: 18,
+      imgPath: Curved_Soprano,
+    },
+    {
+      id: 4,
+      name: "Alto",
+      pitch: "E♭",
+      heightCm: 74,
+      heightIn: 29,
+      imgPath: Alto,
+    },
+    {
+      id: 5,
+      name: "Tenor",
+      pitch: "B♭",
+      heightCm: 94,
+      heightIn: 37,
+      imgPath: Tenor,
+    },
+    {
+      id: 6,
+      name: "Baritone",
+      pitch: "E♭",
+      heightCm: 132,
+      heightIn: 52,
+      imgPath: Baritone,
+    },
+    { id: 7, name: "Bass", pitch: "B♭", imgPath: Bass },
+    {
+      id: 8,
+      name: "Contrabass",
+      pitch: "E♭",
+      heightCm: 200,
+      heightIn: 79,
+      imgPath: Contrabass,
+    },
+    { id: 9, name: "C -", pitch: "C", imgPath: C },
   ]);
 
   function handleSubmit() {}
@@ -41,9 +100,7 @@ function List() {
       <div className="saxTypes__list">
         {saxTypes.map((saxType) => (
           <div className="saxTypes__list__item" key={saxType.id}>
-            <img
-              src={"./SaxPage/images/saxTypes/" + saxType.name + ".png"}
-            ></img>
+            <img src={saxType.imgPath}></img>
             <h3>{saxType.name + " Saxophone"}</h3>
             <ul className="saxTypes__list__item_list  ">
               <li>
