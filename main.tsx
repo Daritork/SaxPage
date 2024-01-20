b15b275cfd9cd1441679df4d894810c104a5c132
Daritork <Daritork@users.noreply.github.com>
2024-01-20 20:01:33 +0000

    deploy: 24899a5f346bd576efd106593596f64fef5e50d5

--- main.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-import List from "./List.tsx";
-import { StrictMode } from "react";
-
-ReactDOM.createRoot(document.getElementById("Types")!).render(
-  <StrictMode>
-    <List />
-  </StrictMode>
-);
