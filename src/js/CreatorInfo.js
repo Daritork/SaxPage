24a93407e81c2a339912eec5ef7fdaf55707783c
Daritork <87564532+Daritork@users.noreply.github.com>
2024-02-04 22:04:50 +0100

    hower effect for "History" (need some changes)

--- src/js/CreatorInfo.js
+++ src/js/CreatorInfo.js
@@ -1,1 +1,7 @@
-function CreatorInfo() {}
+var element = document.getElementById("creatorInfo");
+function CreatorInfoShow() {
+  element.classList.add("show");
+}
+function CreatorInfoHide() {
+  element.classList.remove("show");
+}
