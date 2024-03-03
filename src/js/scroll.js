9e3a94c25aff45050efa67d132f0f4825caf4643
Daritork <87564532+Daritork@users.noreply.github.com>
2024-03-03 19:53:29 +0100

    navigation Changes

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,16 +1,40 @@
-document.querySelector(".Scrollbutton").style.opacity = "0";
+document.getElementById("scroll").addEventListener("click", Scroll);
+document
+  .querySelector("#scrollToWelcome")
+  .addEventListener("click", ScrollToWelcomeTrue);
+document
+  .querySelector("#scrollToSaxophoneParts")
+  .addEventListener("click", ScrollToWelcomeFalse);
+document
+  .querySelector("#scrollToHistory")
+  .addEventListener("click", ScrollToHistory);
+document
+  .querySelector("#scrollToTypesOfSax")
+  .addEventListener("click", ScrollToTypesOfSax);
 
 function Scroll() {
-  const welcome = document.getElementById("Welcome");
-
-  welcome.scrollIntoView(true); // align bottom
+  document.querySelector("#Welcome").scrollIntoView(true);
+}
+function ScrollToWelcomeTrue() {
+  document.querySelector("#Welcome").scrollIntoView(true);
+}
+function ScrollToWelcomeFalse() {
+  document.querySelector("#Welcome").scrollIntoView(false);
+}
+function ScrollToHistory() {
+  document.querySelector("#History").scrollIntoView(true);
+}
+function ScrollToTypesOfSax() {
+  document.querySelector("#Types").scrollIntoView(true);
 }
 
 window.addEventListener("scroll", () => {
   const bodyStyle = document.body.style;
   if (bodyStyle.getPropertyValue("--scroll") > 0.3) {
+    document.querySelector(".Scrollbutton").style.visibility = "visible";
     document.querySelector(".Scrollbutton").style.opacity = "1";
   } else {
+    document.querySelector(".Scrollbutton").style.visibility = "hidden";
     document.querySelector(".Scrollbutton").style.opacity = "0";
   }
 });
