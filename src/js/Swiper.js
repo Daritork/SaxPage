76456f92f0c5deb3cdc09fc24c0f654177cbb029
Daritork <87564532+Daritork@users.noreply.github.com>
2024-04-11 22:42:09 +0200

    swiper unneccesary files deleted

--- src/js/Swiper.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".mySwiper", {
-  direction: "vertical",
-  slidesPerView: 1,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
